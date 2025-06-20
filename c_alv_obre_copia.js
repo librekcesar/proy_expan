344aa4fc20b5349c85b8f9f03abea1b94a3d927e
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-06-20 15:26:25 -0600

    Add files via upload

---
+++ c_alv_obre_copia.js
@@ -0,0 +1,8 @@
+var centro_ideal = {
+"type": "FeatureCollection",
+"name": "c_alv_obre",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -99.119376653253937, 19.399584167907165 ] } }
+]
+}
---
+++ c_bahia_sant_copia.js
@@ -0,0 +1,8 @@
+var tulti = {
+"type": "FeatureCollection",
+"name": "c_bahia_sant",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -99.173200621991114, 19.434819979163201 ] } }
+]
+}
